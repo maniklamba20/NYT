680756ce57d2fe5fa3140ddc5f8b212ae8ab3fbd
Manik Lamba <Manik@maniks-macbook-pro.local>
2017-09-11 15:54:58 -0400

    Initial Commit

---
+++ NYTCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYTCode
+//
+//  Created by Manik Lamba on 9/11/17.
+//  Copyright © 2017 Manik Lamba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
